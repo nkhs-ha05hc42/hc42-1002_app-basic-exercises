91274b42e7e1de8c4b3f3e5530e47e613afcc03f
hc421002 <nkhshc421002@g.neec.ac.jp>
2024-12-23 15:53:40 +0900

    6-1-3

---
+++ 6-1/6-1-3.mjs
@@ -0,0 +1,8 @@
+const a = 10
+const b = 20
+
+console.log("加算 (a + b):", a + b)
+console.log("減算 (a - b):", a - b)
+console.log("乗算 (a * b):", a * b)
+console.log("除算 (a / b):", a / b)
+console.log("剰余 (a % b):", a % b)
