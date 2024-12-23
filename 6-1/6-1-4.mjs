1b44aa08777867abea300c8209827976eeb3a12c
hc421002 <nkhshc421002@g.neec.ac.jp>
2024-12-23 15:53:48 +0900

    6-1-4

---
+++ 6-1/6-1-4.mjs
@@ -0,0 +1,23 @@
+// 1. 変数a1、b1をそれぞれfalseとした場合
+const a1 = false
+const b1 = false
+console.log("a1 && b1:", a1 && b1) // 論理積
+console.log("a1 || b1:", a1 || b1) // 論理和
+console.log("!a1:", !a1)           // 論理否定
+
+// 2. 変数a2、b2をそれぞれtrueとした場合
+const a2 = true
+const b2 = true
+console.log("a2 && b2:", a2 && b2) // 論理積
+console.log("a2 || b2:", a2 || b2) // 論理和
+console.log("!a2:", !a2)           // 論理否定
+
+// 3. 変数a3をtrue、b3をfalseとした場合
+const a3 = true
+const b3 = false
+console.log("a3 && b3:", a3 && b3) // 論理積
+console.log("a3 || b3:", a3 || b3) // 論理和
+console.log("!a3:", !a3)           // 論理否定
+
+// 4. 変数a4をfalse、b4をtrueとした場合
+const a4 = false
