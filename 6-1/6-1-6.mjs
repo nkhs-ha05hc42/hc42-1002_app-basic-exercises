cdd29cb3801e82d0cb1e8b581bb57d30f9c1b431
hc421002 <nkhshc421002@g.neec.ac.jp>
2024-12-23 15:54:06 +0900

    6-1-6

---
+++ 6-1/6-1-6.mjs
@@ -0,0 +1,20 @@
+// 変数aを定義
+const a = 10
+
+// 境界値を設定し、各演算のTrue/Falseとなるパターンを出力
+
+// 大なり (>)
+console.log("a > 10:", a > 10)    // False
+console.log("a > 9:", a > 9)      // True
+
+// 大なりイコール (>=)
+console.log("a >= 10:", a >= 10)  // True
+console.log("a >= 11:", a >= 11)  // False
+
+// 小なり (<)
+console.log("a < 10:", a < 10)    // False
+console.log("a < 11:", a < 11)    // True
+
+// 小なりイコール (<=)
+console.log("a <= 10:", a <= 10)  // True
+console.log("a <= 9:", a <= 9)    // False
