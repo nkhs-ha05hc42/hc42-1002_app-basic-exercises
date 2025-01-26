cf9ef6ebd39b8aa6024ca31e4b6712bf3929d644
hc421002 <nkhshc421002@g.neec.ac.jp>
2025-01-26 22:31:31 +0900

    7-2-3import

---
+++ 7-2/7-2-3_sample1_import.mjs
@@ -0,0 +1,10 @@
+import { multiply, divide } from './mathFunctions.mjs'
+
+const multiplyResult = multiply(6, 7)
+console.log(`Multiplication result: ${multiplyResult}`)
+
+const divideResult = divide(20, 5)
+console.log(`Division result: ${divideResult}`)
+
+const divideByZero = divide(20, 0)
+console.log(`Division by zero result: ${divideByZero}`)
