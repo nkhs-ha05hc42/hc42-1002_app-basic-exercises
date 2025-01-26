ae4536d6f7099dcd8b5fbf56fb942fd82e54229e
hc421002 <nkhshc421002@g.neec.ac.jp>
2025-01-26 22:36:49 +0900

    7-6-2

---
+++ 7-6/7-6-2.mjs
@@ -0,0 +1,24 @@
+function getCurrentTime() {
+    const now = new Date()
+    return now.toTimeString().split(' ')[0]
+}
+
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function execute() {
+    console.log(`[${getCurrentTime()}] start`)
+    console.log(`[${getCurrentTime()}] start`)
+
+    await delay(2000)
+    console.log(`[${getCurrentTime()}] ★`)
+
+    await delay(3000)
+    console.log(`[${getCurrentTime()}] ◆`)
+
+    await delay(2000)
+    console.log(`[${getCurrentTime()}] end`)
+}
+
+execute()
