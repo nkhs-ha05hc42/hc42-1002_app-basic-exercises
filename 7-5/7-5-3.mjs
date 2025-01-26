baa05b16e0675a8db753ce81519325d0d8d7ae0f
hc421002 <nkhshc421002@g.neec.ac.jp>
2025-01-26 22:36:18 +0900

    7-5-3

---
+++ 7-5/7-5-3.mjs
@@ -0,0 +1,24 @@
+function getCurrentTime() {
+    const now = new Date()
+    return now.toTimeString().split(' ')[0]
+}
+
+function delayedOutput(delay, symbol) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`[${getCurrentTime()}] ${symbol}`)
+            resolve()
+        }, delay)
+    })
+}
+
+console.log(`[${getCurrentTime()}] start`)
+console.log(`[${getCurrentTime()}] start`)
+
+delayedOutput(2000, "★") 
+    .then(() => {
+        return delayedOutput(3000, "◆")
+    })
+    .then(() => {
+        return delayedOutput(2000, "end")
+    })
