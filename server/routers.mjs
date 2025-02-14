5ff34cc80b9eff68fed6822967d66afc5f5698b4
hc421002 <nkhshc421002@g.neec.ac.jp>
2025-02-14 16:19:52 +0900

    routers

--- server/routers.mjs
+++ server/routers.mjs
@@ -4,6 +4,12 @@ import path from "path"
 import {
     sample1Controller,
     sample2Controller,
+    q8_1_1Controller,
+    q8_1_2Controller,
+    q8_1_3Controller,
+    q8_1_4Controller,
+    q8_2Controller,
+    q8_3Controller,
  } from "./api/controllers.mjs"
 
 const routers = express.Router()
@@ -18,6 +24,16 @@ routers.get("/api/sample2/:id", sample2Controller.getOneSample2)
 routers.get("/api/sample2", sample2Controller.getAllSample2)
 routers.put("/api/sample2/:id", sample2Controller.putSample2)
 routers.delete("/api/sample2/:id", sample2Controller.deleteSample2)
+routers.get("/api/8-1", q8_1_1Controller.get811)
+routers.post("/api/8-1", q8_1_2Controller.post812)
+routers.put("/api/8-1", q8_1_3Controller.put813)
+routers.delete("/api/8-1", q8_1_4Controller.delete814)
+routers.get("/api/8-2", q8_2Controller.get821)
+routers.post("/api/8-2", q8_2Controller.post822)
+routers.put("/api/8-2", q8_2Controller.put823)
+routers.delete("/api/8-2", q8_2Controller.delete824)
+routers.get("/api/8-3", q8_3Controller.get831)
+routers.get("/api/8-3", q8_3Controller.get832)
 
 // client配下は、ブラウザサイドで実行されるファイル（HTML/JS/CSS/画像など）を配置する
 routers.use(express.static(`${path.resolve()}/client`))
